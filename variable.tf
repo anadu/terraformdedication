2042990bd0026048e23d75c3971804bdb033e484
Reddy <kishore.reddy@harman.com>
2025-12-22 10:37:02 +0530

    test

--- variable.tf
+++ variable.tf
@@ -6,7 +6,7 @@ output "print" {
     value = var.plain
 }
 
-whenever we have the call the variable with a string ithe syntax should be eg:"${var.plain}-text"
+#whenever we have the call the variable with a string ithe syntax should be eg:"${var.plain}-text"
 
 output "synstax" {
     value = "${var.plain}-text"
