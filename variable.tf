57771cfc753197fb5db0c3447a6eb238d4629881
Reddy <kishore.reddy@harman.com>
2025-12-22 10:35:46 +0530

    test

--- variable.tf
+++ variable.tf
@@ -5,3 +5,9 @@ variable "plain" {
 output "print" {
     value = var.plain
 }
+
+whenever we have the call the variable with a string ithe syntax should be eg:"${var.plain}-text"
+
+output "synstax" {
+    value = "${var.plain}-text"
+}
