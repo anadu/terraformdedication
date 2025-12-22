5535e7ad501d4d442e8e04ef53e3c68c1eb1904a
Reddy <kishore.reddy@harman.com>
2025-12-22 08:25:58 +0530

    test

--- dat.tf
+++ dat.tf
@@ -69,7 +69,7 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk12"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
