c7dcbb7d13b65a295a7e203d535a69dff19690ab
Reddy <kishore.reddy@harman.com>
2025-12-23 09:57:21 +0530

    resource_group

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -9,3 +9,10 @@ resource "azurerm_virtual_network" vnet {
     location = azurerm_resource_group.rg.location
     address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
