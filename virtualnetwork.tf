936c78cdf361de241a1d522e51fa66389c659b9b
Reddy <kishore.reddy@harman.com>
2025-12-23 09:50:14 +0530

    test

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
     name = "dedication"
     location =  "West Europe"
 }
+
+resource "azurerm_virtual_network" vnet {
+    name = "vnet"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    address_space       = ["10.0.0.0/16"]
+}
