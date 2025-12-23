93b246aa0a9bb8b8d7e2e49930718c87a4e396a8
Reddy <kishore.reddy@harman.com>
2025-12-23 22:35:48 +0530

    test

--- loop-count/provider.tf
+++ loop-count/provider.tf
@@ -3,7 +3,11 @@ provider "azurerm" {
   subscription_id="a3bdf793-32e4-4604-aa40-ad21d74b1bdd"
 }
 
+resource "null_resource" "test" {
+   count = length(var.demo) 
 
-resource "null_resource" "empty"{
-    count = 10
+}
+
+variable "demo" {
+ default = ["kishore","orange"]
 }
