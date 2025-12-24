a906eca1f81f65fd9160039b5ef99782af910023
Reddy <kishore.reddy@harman.com>
2025-12-24 19:07:52 +0530

    test

--- data.tf
+++
@@ -1,7 +0,0 @@
-data "azurerm_resources" "example" {
-  resource_group_name = "glss"
-}
-
-output "print" {
-    value = data.azurerm_resources.example
-}
--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ provider "azurerm" {
   features {}
   subscription_id="a3bdf793-32e4-4604-aa40-ad21d74b1bdd"
 }
+
+data "azurerm_resources" "example" {
+  resource_group_name = "glss"
+}
+
+output "print" {
+    value = data.azurerm_resources.example
+}
